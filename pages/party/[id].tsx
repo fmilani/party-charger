cc1499c9b14e68967145e5b2b34b6311aa12a495
Felipe Milani <felmilani@gmail.com>
2022-10-03 23:08:55 -0300

    Add party name to title tag in party page

--- pages/party/[id].tsx
+++ pages/party/[id].tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { useCombobox } from "downshift";
 import useSWR, { SWRConfig } from "swr";
+import Head from "next/head";
 
 function getFriendsFilter(inputValue: any) {
   return function friendsFilter(friend: any) {
@@ -119,149 +120,157 @@ const Party = () => {
   );
   if (error) return <div>ERROR =((</div>;
   return (
-    <div className="md:container md:mx-auto px-4">
-      <h1 className="text-3xl font-bold text-center my-8">{party.name}</h1>
-      <h3 className="text-2xl font-bold">Quem confirmou presença</h3>
-      <p className="text-md font-bold mb-2">
-        ({party.attendees.length} pessoas)
-      </p>
-      <div className="flex gap-4 overflow-auto">
-        {party.attendees.map((attendee: any) => (
-          <div
-            key={attendee.email}
-            className={`flex-shrink-0 w-24 p-2 border rounded-lg `}
-          >
-            <h5 className="text-center truncate">{`${attendee.first_name} ${
-              attendee.last_name || ""
-            }`}</h5>
-            <Image
-              src={attendee.photo}
-              width={96}
-              height={96}
-              alt={attendee.name}
-              onClick={() => {
-                router.push(`${router.asPath}/${attendee.email}`);
-              }}
-            />
-            <div className="flex gap-1">
-              <span>{!attendee.answered && "⚠️"}</span>
-              <span>
-                {(party.expenses_created ?? true) && attendee.settled && "✅"}
-              </span>
+    <>
+      <Head>
+        <title>{party.name}</title>
+      </Head>
+      <div className="md:container md:mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center my-8">{party.name}</h1>
+        <h3 className="text-2xl font-bold">Quem confirmou presença</h3>
+        <p className="text-md font-bold mb-2">
+          ({party.attendees.length} pessoas)
+        </p>
+        <div className="flex gap-4 overflow-auto">
+          {party.attendees.map((attendee: any) => (
+            <div
+              key={attendee.email}
+              className={`flex-shrink-0 w-24 p-2 border rounded-lg `}
+            >
+              <h5 className="text-center truncate">{`${attendee.first_name} ${
+                attendee.last_name || ""
+              }`}</h5>
+              <Image
+                src={attendee.photo}
+                width={96}
+                height={96}
+                alt={attendee.name}
+                onClick={() => {
+                  router.push(`${router.asPath}/${attendee.email}`);
+                }}
+              />
+              <div className="flex gap-1">
+                <span>{!attendee.answered && "⚠️"}</span>
+                <span>
+                  {(party.expenses_created ?? true) && attendee.settled && "✅"}
+                </span>
+              </div>
             </div>
+          ))}
+        </div>
+        <div className="text-sm mt-2">
+          Legenda: ⚠️ Falta responder | ✅ Já pagou
+        </div>
+        <div className="mt-4">
+          <p className="w-full">Bora? Coloca seu nome aqui embaixo:</p>
+          <div className="flex items-center gap-2">
+            <ComboBox
+              friends={friends}
+              attendees={party.attendees}
+              selectedItem={newAttendee}
+              onSelect={(selectedItem: any) => setNewAttendee(selectedItem)}
+            />
+            <button
+              type="button"
+              onClick={async () => {
+                await fetch(`/api/party/${router.query.id}/attendee`, {
+                  method: "POST",
+                  body: JSON.stringify(newAttendee),
+                });
+                setNewAttendee({ first_name: "", email: "" });
+                router.push(`${router.asPath}/${newAttendee.email}`);
+              }}
+              disabled={newAttendee.email === ""}
+              className="disabled:bg-inherit disabled:border disabled:border-gray-200 disabled:text-gray-400 disabled:hover:bg-gray-200 cursor-pointer hover:bg-indigo-500/10 text-white bg-indigo-500 px-4 py-2 rounded-lg"
+            >
+              Bora!
+            </button>
           </div>
-        ))}
-      </div>
-      <div className="text-sm mt-2">
-        Legenda: ⚠️ Falta responder | ✅ Já pagou
-      </div>
-      <div className="mt-4">
-        <p className="w-full">Bora? Coloca seu nome aqui embaixo:</p>
-        <div className="flex items-center gap-2">
-          <ComboBox
-            friends={friends}
-            attendees={party.attendees}
-            selectedItem={newAttendee}
-            onSelect={(selectedItem: any) => setNewAttendee(selectedItem)}
-          />
+        </div>
+        <div className={router.query.host ? "visible" : "hidden"}>
+          <h3 className="text-2xl font-bold mb-2 mt-8">Despesas</h3>
+          <div className="flex items-end gap-2">
+            <label className="block flex-1">
+              <span>Descrição</span>
+              <input
+                id="newExpenseDescription"
+                name="newExpenseDescription"
+                value={newExpense?.description}
+                onChange={(e) =>
+                  setNewExpense({
+                    ...newExpense,
+                    description: e.target.value,
+                  })
+                }
+                className="mt-1 block w-full border-inherit rounded-lg"
+                type="text"
+              />
+            </label>
+            <label className="block shrink w-20">
+              <span>Valor</span>
+              <input
+                id="newExpenseValue"
+                name="newExpenseValue"
+                value={newExpense?.value}
+                onChange={(e) =>
+                  setNewExpense({ ...newExpense, value: e.target.value })
+                }
+                type="number"
+                className="mt-1 block w-full border-inherit rounded-lg"
+              />
+            </label>
+            <button
+              type="button"
+              onClick={async () => {
+                await fetch(`/api/party/${router.query.id}/expense`, {
+                  method: "POST",
+                  body: JSON.stringify(newExpense),
+                });
+                setNewExpense({ description: "", value: "" });
+                mutate({
+                  friends,
+                  party: {
+                    ...party,
+                    expenses: [newExpense, ...party.expenses],
+                  },
+                });
+              }}
+              disabled={!newExpense.description || !newExpense.value}
+              className="disabled:border-gray-200 disabled:text-gray-400 disabled:hover:bg-gray-200 cursor-pointer hover:bg-indigo-500/10 border border-indigo-500 px-4 py-2 rounded-lg"
+            >
+              Adicionar
+            </button>
+          </div>
+        </div>
+        <h3 className="text-2xl font-bold mt-8">Quanto custou</h3>
+        <p className="text-md font-bold mb-2">
+          (
+          {`${party.expenses.reduce(
+            (acc: number, cur: { value: string }) => acc + Number(cur.value),
+            0
+          )} no total`}
+          )
+        </p>
+        <ul className="border rounded-lg p-1 max-w-sm">
+          {party.expenses.map((expense: any) => (
+            <li key={expense.description} className="flex justify-between">
+              <span className="truncate">{expense.description}</span>
+              <span>{expense.value}</span>
+            </li>
+          ))}
+        </ul>
+        <div className="text-center py-4">
           <button
-            type="button"
             onClick={async () => {
-              await fetch(`/api/party/${router.query.id}/attendee`, {
-                method: "POST",
-                body: JSON.stringify(newAttendee),
-              });
-              setNewAttendee({ first_name: "", email: "" });
-              router.push(`${router.asPath}/${newAttendee.email}`);
+              fetch(`/api/party/${router.query.id}/charge`, { method: "POST" });
             }}
-            disabled={newAttendee.email === ""}
-            className="disabled:bg-inherit disabled:border disabled:border-gray-200 disabled:text-gray-400 disabled:hover:bg-gray-200 cursor-pointer hover:bg-indigo-500/10 text-white bg-indigo-500 px-4 py-2 rounded-lg"
+            className="m-auto text-white bg-indigo-500 disabled:bg-gray-200 px-16 py-4 rounded-lg flex-none"
+            disabled={!router.query.host}
           >
-            Bora!
+            Fazer as contas
           </button>
         </div>
       </div>
-      <div className={router.query.host ? "visible" : "hidden"}>
-        <h3 className="text-2xl font-bold mb-2 mt-8">Despesas</h3>
-        <div className="flex items-end gap-2">
-          <label className="block flex-1">
-            <span>Descrição</span>
-            <input
-              id="newExpenseDescription"
-              name="newExpenseDescription"
-              value={newExpense?.description}
-              onChange={(e) =>
-                setNewExpense({
-                  ...newExpense,
-                  description: e.target.value,
-                })
-              }
-              className="mt-1 block w-full border-inherit rounded-lg"
-              type="text"
-            />
-          </label>
-          <label className="block shrink w-20">
-            <span>Valor</span>
-            <input
-              id="newExpenseValue"
-              name="newExpenseValue"
-              value={newExpense?.value}
-              onChange={(e) =>
-                setNewExpense({ ...newExpense, value: e.target.value })
-              }
-              type="number"
-              className="mt-1 block w-full border-inherit rounded-lg"
-            />
-          </label>
-          <button
-            type="button"
-            onClick={async () => {
-              await fetch(`/api/party/${router.query.id}/expense`, {
-                method: "POST",
-                body: JSON.stringify(newExpense),
-              });
-              setNewExpense({ description: "", value: "" });
-              mutate({
-                friends,
-                party: { ...party, expenses: [newExpense, ...party.expenses] },
-              });
-            }}
-            disabled={!newExpense.description || !newExpense.value}
-            className="disabled:border-gray-200 disabled:text-gray-400 disabled:hover:bg-gray-200 cursor-pointer hover:bg-indigo-500/10 border border-indigo-500 px-4 py-2 rounded-lg"
-          >
-            Adicionar
-          </button>
-        </div>
-      </div>
-      <h3 className="text-2xl font-bold mt-8">Quanto custou</h3>
-      <p className="text-md font-bold mb-2">
-        (
-        {`${party.expenses.reduce(
-          (acc: number, cur: { value: string }) => acc + Number(cur.value),
-          0
-        )} no total`}
-        )
-      </p>
-      <ul className="border rounded-lg p-1 max-w-sm">
-        {party.expenses.map((expense: any) => (
-          <li key={expense.description} className="flex justify-between">
-            <span className="truncate">{expense.description}</span>
-            <span>{expense.value}</span>
-          </li>
-        ))}
-      </ul>
-      <div className="text-center py-4">
-        <button
-          onClick={async () => {
-            fetch(`/api/party/${router.query.id}/charge`, { method: "POST" });
-          }}
-          className="m-auto text-white bg-indigo-500 disabled:bg-gray-200 px-16 py-4 rounded-lg flex-none"
-          disabled={!router.query.host}
-        >
-          Fazer as contas
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 export default Page;
