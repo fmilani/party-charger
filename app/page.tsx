8a7e958baf64d79937b544c25cb40c7acc66f2fe
Felipe Milani <felmilani@gmail.com>
2022-11-07 00:42:15 -0300

    forgot to change 'brand' color on landing page lol

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="h-full flex flex-col">
       <div className="flex-1 flex flex-col items-center justify-center text-center">
-        <span className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-rose-600">
+        <span className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-700 to-teal-600">
           Faça as contas da sua festa com facilidade
         </span>
         <Link
